9da09d821e97976c90dc057488cdfd8e3cdc4ee8
Edeki Okoh <Okohedeki@gmail.com>
2024-08-01 16:31:04 -0700

    Checking in Test for PR workflow

--- northwind_dbt_project/stored_procedures/Insert_orders.sql
+++ northwind_dbt_project/stored_procedures/Insert_orders.sql
@@ -5,6 +5,6 @@ BEGIN
     INSERT INTO Orders (CustomerID, EmployeeID, OrderDate, RequiredDate, ShippedDate, ShipVia, Freight, ShipName, ShipAddress, ShipCity, ShipRegion, ShipPostalCode, ShipCountry)
     VALUES 
     ('Edeki', 1, '2024-07-30', '2024-08-05', NULL, 1, 32.38, 'Alfreds Futterkiste', 'Obere Str. 57', 'Berlin', NULL, '12209', 'Germany'),
-    ('ANATR', 3, '2024-07-25', '2024-08-01', '2024-07-29', 2, 11.61, 'Ana Trujillo Emparedados y helados', 'Avda. de la Constitución 2222', 'México D.F.', NULL, '05021', 'Mexico'),
+    ('BRIAN', 3, '2024-07-25', '2024-08-01', '2024-07-29', 2, 11.61, 'Ana Trujillo Emparedados y helados', 'Avda. de la Constitución 2222', 'México D.F.', NULL, '05021', 'Mexico'),
     ('ANTON', 4, '2024-07-20', '2024-07-27', '2024-07-22', 3, 45.34, 'Antonio Moreno Taquería', 'Mataderos 2312', 'México D.F.', NULL, '05023', 'Mexico');
 END;
